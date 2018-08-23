f413c6ef3ce89b2292a02ab89ac0fcd65435817c
Christian Brunotte <cb@lathspell.de>
2018-08-24 00:05:41 +0200

    correction

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,11 +1,10 @@
 object HandshakeCalculator {
     fun calculateHandshake(i: Int): List<Signal> = mutableListOf<Signal>().apply {
-        when {
-            i and 1 == 1 -> add(Signal.WINK)
-            i and 2 == 1 -> add(Signal.DOUBLE_BLINK)
-            i and 4 == 1 -> add(Signal.CLOSE_YOUR_EYES)
-            i and 8 == 1 -> add(Signal.JUMP)
-            i and 16 == 1 -> reverse()
-        }
+        if (i and 1 == 1) add(Signal.WINK)
+        if (i and 2 == 2) add(Signal.DOUBLE_BLINK)
+        if (i and 4 == 4) add(Signal.CLOSE_YOUR_EYES)
+        if (i and 8 == 8) add(Signal.JUMP)
+        if (i and 16 == 16) reverse()
     }
 }
+
