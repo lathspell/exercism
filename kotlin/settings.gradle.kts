bf4748bc132b963b0f633a2b3c13d64360a1d0d4
Christian Brunotte <cb@lathspell.de>
2019-05-19 18:13:32 +0200

    more excludes

--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -2,5 +2,5 @@ rootProject.name = "Exercism Kotlin Track"
 
 file("./").listFiles()
   .filter { it.isDirectory }
-  .filter { it.name !in listOf(".idea", ".gradle") }
+  .filter { it.name !in listOf(".idea", ".gradle", "gradle", "build", "out") }
   .forEach { include(it.name) }
