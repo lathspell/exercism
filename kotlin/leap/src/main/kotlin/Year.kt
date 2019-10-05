62849213d97b8bfc7d646412060c7b8e9968d073
Christian Brunotte <cb@lathspell.de>
2019-10-05 22:55:38 +0200

    refreshing

--- kotlin/leap/src/main/kotlin/Year.kt
+++ kotlin/leap/src/main/kotlin/Year.kt
@@ -1,10 +1,13 @@
-// changed: switched from single-line to "when" as seen in GeekTeam's solution as it's easier to grasp
 class Year(private val year: Int) {
-
     val isLeap: Boolean = when {
-            year % 400 == 0 -> true
-            year % 100 == 0 -> false
-            year % 4 == 0 -> true
-            else -> false
-        }
+        year % 400 == 0 -> true
+        year % 100 == 0 -> false
+        year % 4 == 0 -> true
+        else -> false
+    }
+}
+
+// Works fine but maybe not so readable
+class Year_oldschool(year: Int) {
+    val isLeap: Boolean = (year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0))
 }
