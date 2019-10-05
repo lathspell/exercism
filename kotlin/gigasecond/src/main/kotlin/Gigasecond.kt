e38be82a023fcc3a308c8f29eff3030606a3a4fb
Christian Brunotte <cb@lathspell.de>
2019-10-05 23:54:48 +0200

    refreshing

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -3,7 +3,7 @@ import java.time.LocalDateTime
 
 class Gigasecond(birth: LocalDateTime) {
 
-    val date = birth.plusSeconds(1_000_000_000)
+    val date : LocalDateTime = birth.plusSeconds(1_000_000_000)
 
     constructor(birth: LocalDate) : this(birth.atStartOfDay())
 }
