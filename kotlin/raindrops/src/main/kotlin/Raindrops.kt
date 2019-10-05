5089e6689ea4e3b26deeee17eef7045d9de7d830
Christian Brunotte <cb@lathspell.de>
2019-10-06 00:53:18 +0200

    refreshing

--- kotlin/raindrops/src/main/kotlin/Raindrops.kt
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -1,4 +1,3 @@
-// changed: switched to buildString as seen in Nexcius's solution
 class Raindrops {
     companion object {
         fun convert(n: Int) = buildString {
@@ -7,5 +6,13 @@ class Raindrops {
             if ((n % 7) == 0) append("Plong")
             if (isBlank()) append(n)
         }
+
+        // works but not as nice as buildString()
+        fun convert2(i: Int): String {
+            fun checkFactor(i: Int, factor: Int, word: String) = if ((i % factor) == 0) word else ""
+            val output = checkFactor(i, 3, "Pling") + checkFactor(i, 5, "Plang") + checkFactor(i, 7, "Plong")
+            return if (output.isEmpty()) i.toString() else output
+        }
+
     }
 }
