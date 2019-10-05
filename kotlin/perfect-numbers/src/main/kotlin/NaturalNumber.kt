50866cb1fc385eb025e6c0b3e640100dd87c8cc5
Christian Brunotte <cb@lathspell.de>
2019-10-06 01:27:54 +0200

    refreshing

--- kotlin/perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ kotlin/perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -3,14 +3,11 @@ enum class Classification {
 }
 
 fun classify(n: Int): Classification {
-    require(n > 0)
-    val sum = (n / 2 downTo 1).filter { n % it == 0 }.sum()
+    require(n > 0) { "Number must be natural!" }
+    val sum = (1..n / 2).filter { n % it == 0 }.sum()
     return when {
         sum > n -> Classification.ABUNDANT
         sum < n -> Classification.DEFICIENT
         else -> Classification.PERFECT
     }
 }
-
-
-
