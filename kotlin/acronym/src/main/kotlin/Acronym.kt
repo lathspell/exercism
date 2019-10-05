dc862d7943bbad37835f16950aae01658ba8a6a1
Christian Brunotte <cb@lathspell.de>
2019-10-06 00:35:01 +0200

    refreshing

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -4,5 +4,13 @@ class Acronym {
                 .splitToSequence(" ", "-")
                 .map { it[0].toUpperCase() }
                 .joinToString("")
+
+        // works for any separator not only space and dash
+        fun generate2(s: String) = s
+                .toUpperCase()
+                .split(Regex("[^A-Z]"))
+                .filter { it -> it.any { it2 -> it2 in 'A'..'Z'}  }
+                .map { it[0] }
+                .joinToString("")
     }
 }
