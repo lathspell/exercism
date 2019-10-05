397cd74753d30a7110a5c6e013cfa978fad1f9c8
Christian Brunotte <cb@lathspell.de>
2019-10-05 23:40:46 +0200

    refreshing

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,9 +1,21 @@
-// changed: if+throw to require; class to object; indices instead of (0..a.length)
-object Hamming {
+class Hamming {
+    companion object {
 
-    fun compute(a: String, b: String): Int {
-        require(a.length == b.length) { "left and right strands must be of equal length." }
-        return a.indices.count { a[it] != b[it] }
+        fun compute(a: String, b: String): Int {
+            require(a.length == b.length) { "left and right strands must be of equal length." }
+            return a.indices.count { a[it] != b[it] }
+        }
+
+        // works, too
+        fun compute3(a: String, b: String): Int {
+            require(a.length == b.length) { "left and right strands must be of equal length." }
+            return a.filterIndexed { i, c -> b[i] == c }.count()
+        }
+
+        // works but I find the !it not very readable
+        fun compute2(a: String, b: String): Int {
+            require(a.length == b.length) { "left and right strands must be of equal length." }
+            return a.mapIndexed { i, c -> b[i] == c }.count { !it }
+        }
     }
-
 }
