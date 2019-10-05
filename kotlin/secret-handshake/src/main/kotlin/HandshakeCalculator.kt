3dea3b43960fdf3b8ca8b5026f8397a9c9e31366
Christian Brunotte <cb@lathspell.de>
2019-10-06 01:10:12 +0200

    refreshing

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -6,5 +6,15 @@ object HandshakeCalculator {
         if (i and 8 == 8) add(Signal.JUMP)
         if (i and 16 == 16) reverse()
     }
-}
 
+    // works but no functional enough
+    fun calculateHandshake2(i: Int): List<Signal> {
+        val result = ArrayList<Signal>()
+        if (i and 1 == 1) result.add(Signal.WINK)
+        if (i and 2 == 2) result.add(Signal.DOUBLE_BLINK)
+        if (i and 4 == 4) result.add(Signal.CLOSE_YOUR_EYES)
+        if (i and 8 == 8) result.add(Signal.JUMP)
+        if (i and 16 == 16) result.reverse()
+        return result
+    }
+}
