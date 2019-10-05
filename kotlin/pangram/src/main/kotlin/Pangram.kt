3f50d471187346f847907b38f1cab10af90c4f63
Christian Brunotte <cb@lathspell.de>
2019-10-05 23:27:06 +0200

    refreshing

--- kotlin/pangram/src/main/kotlin/Pangram.kt
+++ kotlin/pangram/src/main/kotlin/Pangram.kt
@@ -1,11 +1,23 @@
 // changed: removed unused variable "abc"
 // changed: using "run()" to get rid of temporary variable and "return"
 class Pangram {
-    companion object {
 
+    companion object {
         fun isPangram(s: String): Boolean =
                 s.toLowerCase().run {
                     ('a'..'z').all { c -> contains(c) }
                 }
+
+        // works but looks not very functional
+        fun isPangram3(s: String): Boolean {
+            val hits = ('a'..'z').associateWith { false }.toMutableMap()
+            s.toLowerCase().forEach { hits[it] = true }
+            return hits.none { !it.value }
+        }
+
+        // works but algorithm does too much
+        fun isPangram2(s: String): Boolean {
+            return s.toLowerCase().filterNot { it < 'a' || it > 'z' }.toList().sorted().distinct() == ('a'..'z').toList()
+        }
     }
 }
