e4ef2e3269c3677cf7e2c4e17f8219bc75c67415
Christian Brunotte <cb@lathspell.de>
2019-10-06 00:20:36 +0200

    refreshing

--- kotlin/space-age/src/main/kotlin/SpaceAge.kt
+++ kotlin/space-age/src/main/kotlin/SpaceAge.kt
@@ -1,15 +1,16 @@
-class SpaceAge(private val age: Long) {
+import kotlin.math.round
 
-    private val earthSeconds = 31557600
+class SpaceAge(private val earthSecs: Long) {
+    fun onEarth(): Double = calcTo2(1.0)
+    fun onMercury(): Double = calcTo2(0.2408467)
+    fun onVenus(): Double = calcTo2(0.61519726)
+    fun onMars(): Double = calcTo2(1.8808158)
+    fun onJupiter(): Double = calcTo2(11.862615)
+    fun onSaturn(): Double = calcTo2(29.447498)
+    fun onUranus(): Double = calcTo2(84.016846)
+    fun onNeptune(): Double = calcTo2(164.79132)
 
-    private fun calc(x: Double) = Math.round(age / x / earthSeconds * 100) / 100.0
-
-    fun onEarth() = calc(1.0)
-    fun onMercury() = calc(0.2408467)
-    fun onVenus() = calc(0.61519726)
-    fun onMars() = calc(1.8808158)
-    fun onJupiter() = calc(11.862615)
-    fun onSaturn() = calc(29.447498)
-    fun onUranus() = calc(84.016846)
-    fun onNeptune() = calc(164.79132)
+    private val earthSecsPerYear = 31557600.0
+    private fun calcTo2(x: Double) = roundTo2(earthSecs / earthSecsPerYear / x)
+    private fun roundTo2(d: Double) = round(d * 100) / 100.0
 }
