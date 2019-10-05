d21ee49faa1fab8182214eaaa75b5f924963a431
Christian Brunotte <cb@lathspell.de>
2019-10-06 01:00:47 +0200

    refreshing

--- kotlin/difference-of-squares/src/main/kotlin/Squares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,7 +1,7 @@
 // changed: replaced "sum(a-b)" by "b-a" :-)
 // Have a look at leetwinski's solution for a math formula that replaces the sum loops!
 class Squares(private val nr: Int) {
-    fun squareOfSum() = (1..nr).sum().let { it * it }
+    fun squareOfSum() = (1..nr).sum().let { it * it } // why is there now power() method?
     fun sumOfSquares() = (1..nr).sumBy { it * it }
     fun difference() = squareOfSum() - sumOfSquares()
 }
