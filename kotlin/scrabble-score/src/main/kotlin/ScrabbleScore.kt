e9387fa6e01acda3e64096e9309d205ba9820a7d
Christian Brunotte <cb@lathspell.de>
2019-10-06 00:40:13 +0200

    refreshing

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -2,16 +2,18 @@
 // changed: use object instead of class and companion object
 object ScrabbleScore {
 
-    fun scoreWord(s: String) = s.toUpperCase().sumBy {
-        when (it) {
-            in "AEIOULNRST" -> 1
-            in "DG" -> 2
-            in "BCMP" -> 3
-            in "FHVWY" -> 4
-            in "K" -> 5
-            in "JX" -> 8
-            in "QZ" -> 10
-            else -> 0
-        }
-    }
+    fun scoreWord(s: String) = s
+            .toUpperCase()
+            .sumBy {
+                when (it) {
+                    in "AEIOULNRST" -> 1
+                    in "DG" -> 2
+                    in "BCMP" -> 3
+                    in "FHVWY" -> 4
+                    in "K" -> 5
+                    in "JX" -> 8
+                    in "QZ" -> 10
+                    else -> 0
+                }
+            }
 }
