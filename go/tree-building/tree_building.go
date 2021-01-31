432b6ac6063f0aec185069f604f05e14da1b8375
Christian Brunotte <cb@lathspell.de>
2021-01-31 17:18:56 +0100

    all tests green

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -1,35 +1,87 @@
 // Package tree builds a tree structure from records
 package tree
 
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
+
 // Record represents a single record like a message
 type Record struct {
-	ID int
+	ID     int
 	Parent int
 }
 
 // Node is the component that forms the tree with Id referencing Records
 type Node struct {
-	ID int
+	ID       int
 	Children []*Node
 }
 
 // Build creates a tree from a given set of Records
 func Build(records []Record) (*Node, error) {
-	var first *Node
-	for _,r := range records {
-		first = insert(&r, first)
+	fmt.Printf("\n=== Build(%v) ===\n", records)
+
+	// Sort records so that we always have the trunk before its leaves
+	sort.SliceStable(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+
+	if len(records) > 0 && records[0].ID != records[0].Parent {
+		return nil, errors.New("expect first root to have ID == Parent")
 	}
-	return first, nil
+
+	var root *Node
+	var error error
+	var lastID = -1
+	for _, r := range records {
+
+		if r.ID != lastID + 1 {
+			return nil, errors.New("duplicate or non continuous record")
+		}
+		lastID++
+
+		root, error = insert(root, &r)
+		if error != nil {
+			return nil, error
+		}
+	}
+
+	return root, nil
 }
 
-func insert(record *Record, first *Node) *Node {
-	if (first == nil) {
-		first = &Node{ ID : record.ID }
+func insert(root *Node, record *Record) (*Node, error) {
+	// check root
+	if root == nil {
+		root = &Node{ID: record.ID}
+		return root, nil
 	}
 
-	if first.ID == record.Parent {
-		first = append(first.Children, record)
+	// find parent
+	var parent = findNode(root, record.Parent)
+	if parent == nil {
+		return nil, errors.New("no parent")
 	}
 
-	return first
+	// append record to just found parent
+	parent.Children = append(parent.Children, &Node{ID: record.ID})
+
+	// always return root, not parent
+	return root, nil
+}
+
+func findNode(node *Node, wanted int) *Node {
+	if node.ID == wanted {
+		return node
+	}
+
+	for _, child := range node.Children {
+		var found = findNode(child, wanted)
+		if found != nil {
+			return found
+		}
+	}
+
+	return nil
 }
