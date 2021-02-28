9d131bb02a5fece4169e2229c0d7377529acabeb
Christian Brunotte <cb@lathspell.de>
2021-02-28 17:09:05 +0100

    from https://github.com/exercism/kotlin/pull/374

--- kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
+++ kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -1,16 +1,22 @@
-class KindergartenGarden(private val garden: String) {
+class KindergartenGarden(private val diagram: String) {
 
     val students = listOf(
             "Alice", "Bob", "Charlie", "David",
             "Eve", "Fred", "Ginny", "Harriet",
             "Ileana", "Joseph", "Kincaid", "Larry")
 
-    fun getPlantsOfStudent(student: String): List<Plant> {
+    val plants: Map<Char, String> = mapOf(
+            'V' to "violets",
+            'R' to "radishes",
+            'C' to "clover",
+            'G' to "grass")
+
+    fun getPlantsOfStudent(student: String): List<String> {
         val idx = students.indexOf(student)
-        return garden
-                .split("\n")
-                .map { it.substring(idx * 2..idx * 2 + 1) }
-                .joinToString("")
-                .map { Plant.getPlant(it) }
+        return diagram
+                .split("\n") // split into two lines
+                .map { it.substring(idx * 2, idx * 2 + 2) } // the two letters of that student in each line
+                .joinToString("") // merge the two letters of each diagram line into one string
+                .map { plants[it]!! } // lookup the plant names by the given letters
     }
 }
