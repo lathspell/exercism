e24621b6ba718a028d7c01dca95a0a0934ee71bf
Christian Brunotte <cb@lathspell.de>
2021-02-28 17:16:41 +0100

    from https://github.com/exercism/kotlin/pull/319

--- kotlin/diffie-hellman/src/main/kotlin/DiffieHellman.kt
+++ kotlin/diffie-hellman/src/main/kotlin/DiffieHellman.kt
@@ -1,7 +1,7 @@
 import java.math.BigInteger
 import java.util.Random
 
-class DiffieHellman {
+object DiffieHellman {
 
     fun privateKey(prime: BigInteger): BigInteger {
         val rnd = Random()
