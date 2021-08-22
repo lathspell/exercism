9f77526e2e864c4d469b635a4dcb5b9ed0661399
Christian Brunotte <cb@lathspell.de>
2021-08-22 15:42:10 +0200

    solved triangle

---
+++ go/triangle/triangle.go
@@ -0,0 +1,37 @@
+package triangle
+
+import (
+	"math"
+)
+
+type Kind string
+
+const (
+	NaT = "not a triangle"
+	Equ = "equilateral"
+	Iso = "isosceles"
+	Sca = "scalene"
+)
+
+func KindFromSides(a, b, c float64) Kind {
+	// all sides must have a positive and finite length
+	if math.IsNaN(a) || a <= 0 || math.IsInf(a, 1) ||
+		math.IsNaN(b) || b <= 0 || math.IsInf(b, 1) ||
+		math.IsNaN(c) || c <= 0 || math.IsInf(c, 1) {
+		return NaT
+	}
+
+	// every two sides must in sum be greater than the remaining one
+	if a+b < c || b+c < a || c+a < b {
+		return NaT
+	}
+
+	switch {
+	case a == b && b == c:
+		return Equ
+	case a == b || b == c || a == c:
+		return Iso
+	default:
+		return Sca
+	}
+}
